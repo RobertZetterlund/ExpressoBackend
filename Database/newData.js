6c9177fe1e4ff5f4345ca8b919df2b8527f5b01e
RobertZetterlund <31474146+RobertZetterlund@users.noreply.github.com>
2019-05-17 18:08:21 +0200

    added descriptions

--- Database/newData.js
+++ Database/newData.js
@@ -1,142 +1,162 @@
 const brewed_coffee = {
-    name: "Brewed Coffee",
-    price: 12,
-    volume: 330,
-    id: 123
-  };
-  
-  const cappuccino = {
-    name: "Cappuccino",
-    price: 28,
-    volume: 500,
-    id: 124
-  };
-  
-  const latte = {
-    name: "Caffee Latte",
-    price: 28,
-    volume: 500,
-    id: 125
-  };
-  
-  const biblioteket = {
-    id: 0,
-    name: "Biblioteket",
-    // picture: require("./resources/biblan.js"),
-    coordinates: {
-      latitude: 57.690382,
-      longitude: 11.978556
+  name: "Brygg Kaffe",
+  price: 12,
+  volume: 330,
+  id: 123
+};
+
+const cappuccino = {
+  name: "Cappuccino",
+  price: 28,
+  volume: 500,
+  id: 124
+};
+
+const latte = {
+  name: "Caffee Latte",
+  price: 28,
+  volume: 500,
+  id: 125
+};
+
+const biblioteket = {
+  id: 0,
+  name: "Biblioteket",
+  // picture: require("./resources/biblan.js"),
+  coordinates: {
+    latitude: 57.690382,
+    longitude: 11.978556
+  },
+  street: "Hörsalsvägen 2",
+  drinkList: [
+    {
+      ...brewed_coffee,
+      description: "Chalmers bästa kaffe för studerande",
+      shopId: 0
     },
-    drinkList: [
-      {
-        ...brewed_coffee,
-        shopId: 0
-      },
-      {
-        ...latte,
-        shopId: 0
-      },
-      {
-        ...cappuccino,
-        shopId: 0
-      }
-    ]
-  };
-  
-  const bulten = {
-    id: 1,
-    name: "Bulten",
-    // picture: require("./resources/bulten.jpg"),
-    coordinates: {
-      latitude: 57.689008,
-      longitude: 11.978538
+    {
+      ...latte,
+      description: "Lätt en latte!",
+      shopId: 0
     },
-    drinkList: [
-      {
-        ...brewed_coffee,
-        shopId: 1
-      },
-      {
-        ...cappuccino,
-        shopId: 1
-      }
-    ]
-  };
-  
-  const linsen = {
-    id: 2,
-    name: "Linsen",
-    // picture: require("./resources/linsen.jpg"),
-    coordinates: {
-      latitude: 57.687962,
-      longitude: 11.978813
+    {
+      ...cappuccino,
+      description: "Inte kaps men en cappucino!",
+      shopId: 0
+    }
+  ]
+};
+
+const bulten = {
+  id: 1,
+  name: "Bulten",
+  // picture: require("./resources/bulten.jpg"),
+  coordinates: {
+    latitude: 57.689008,
+    longitude: 11.978538
+  },
+  street: "Hörsalsvägen 7",
+  drinkList: [
+    {
+      ...brewed_coffee,
+      description: "Så gott som hemmabryggt",
+      shopId: 1
     },
-    drinkList: [
-      {
-        ...brewed_coffee,
-        shopId: 2
-      },
-      {
-        ...latte,
-        shopId: 2
-      },
-      {
-        ...cappuccino,
-        shopId: 2
-      }
-    ]
-  };
-  
-  const veras_cafe = {
-    id: 3,
-    name: "Veras Café",
-    // picture: require("./resources/vera.jpg"),
-    coordinates: {
-      latitude: 57.693158,
-      longitude: 11.975036
+    {
+      ...cappuccino,
+      description: "Inte kaps men en cappucino!",
+
+      shopId: 1
+    }
+  ]
+};
+
+const linsen = {
+  id: 2,
+  name: "Linsen",
+  street: "Hörsalsvägen 11",
+  // picture: require("./resources/linsen.jpg"),
+  coordinates: {
+    latitude: 57.687962,
+    longitude: 11.978813
+  },
+  drinkList: [
+    {
+      ...brewed_coffee,
+      description: "Hemmabryggt",
+
+      shopId: 2
     },
-    drinkList: [
-      {
-        ...brewed_coffee,
-        shopId: 3
-      },
-      {
-        ...latte,
-        shopId: 3
-      },
-      {
-        ...cappuccino,
-        shopId: 3
-      }
-    ]
-  };
-  
-  const wijkanders = {
-    id: 4,
-    name: "Wijkanders",
-    // picture: require("./resources/wijkanders.jpg"),
-    coordinates: {
-      latitude: 57.692538,
-      longitude: 11.97539
+    {
+      ...latte,
+      description: "DataFavoriten",
+      shopId: 2
+
     },
-    drinkList: [
-      {
-        ...brewed_coffee,
-        shopId: 4
-      },
-      {
-        ...latte,
-        shopId: 4
-      }
-    ]
-  };
-  
-  const shops = [biblioteket, bulten, linsen, veras_cafe, wijkanders];
-  
-  const coffeeSorts = [brewed_coffee, cappuccino, latte];
-  
-  module.exports = {
-    shops: shops,
-    coffeeSorts: coffeeSorts
-  };
-  
+    {
+      ...cappuccino,
+      description: "IT-Favoriten",
+      shopId: 2
+    }
+  ]
+};
+
+const veras_cafe = {
+  id: 3,
+  name: "Veras Café",
+  street: "Vera Sandbergs allé",
+  // picture: require("./resources/vera.jpg"),
+  coordinates: {
+    latitude: 57.693158,
+    longitude: 11.975036
+  },
+  drinkList: [
+    {
+      ...brewed_coffee,
+      description: "I-arnas räddning",
+      shopId: 3
+    },
+    {
+      ...latte,
+      description: "AE-arnas räddning",
+      shopId: 3
+    },
+    {
+      ...cappuccino,
+      description: "Ingens räddning",
+      shopId: 3
+    }
+  ]
+};
+
+const wijkanders = {
+  id: 4,
+  name: "Wijkanders",
+  street: "Vera Sandbergs allé 5B",
+  // picture: require("./resources/wijkanders.jpg"),
+  coordinates: {
+    latitude: 57.692538,
+    longitude: 11.97539
+  },
+  drinkList: [
+    {
+      ...brewed_coffee,
+      shopId: 4,
+      description: "Wiljkanders bästa"
+    },
+    {
+      ...latte,
+      description: "Lätt en Latte",
+      shopId: 4
+    }
+  ]
+};
+
+const shops = [biblioteket, bulten, linsen, veras_cafe, wijkanders];
+
+const coffeeSorts = [brewed_coffee, cappuccino, latte];
+
+module.exports = {
+  shops: shops,
+  coffeeSorts: coffeeSorts
+};
